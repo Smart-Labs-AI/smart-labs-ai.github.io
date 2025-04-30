137c87f6415a90eccb3a4ced2a07e311e9af7013
Stefan Fenn <stefan.fenn@smart-cybersecurity.de>
2025-04-30 21:09:36 +0000

    exclude fa explicitly

--- website/postcss.config.js
+++ website/postcss.config.js
@@ -1,14 +1,27 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
+const path = require('path')
 
-module.exports = {
-  plugins: [
-    purgecss({
-      content: ['./layouts/**/*.html', './content/**/*.md'],
-      safelist: [
-        'call-to-action',
-        'title'
-      ],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    })
-  ]
+const excludedFiles = [
+  path.resolve(__dirname, 'static/css/fontawesome/6.7.2/css/all.min.css')
+]
+
+module.exports = (ctx) => {
+  const filePath = ctx.file ? path.resolve(ctx.file) : ''
+
+  const purgeEnabled = !excludedFiles.includes(filePath)
+
+  return {
+    plugins: [
+      ...(purgeEnabled ? [
+        purgecss({
+          content: ['./layouts/**/*.html', './content/**/*.md'],
+          safelist: [
+            'call-to-action', 
+            'title'
+          ],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        })
+      ] : [])
+    ]
+  }
 }
